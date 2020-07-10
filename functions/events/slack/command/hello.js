4ebbf92ee0de6ecc72c9fed6ad5f3593e6aae45d
Jacob <jacoblee93@gmail.com>
2020-07-10 10:09:16 -0700

    Initial commit

---
+++ functions/events/slack/command/hello.js
@@ -0,0 +1,28 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+/**
+* An HTTP endpoint that acts as a webhook for Slack command event
+* @param {object} event
+* @returns {object} result Your return value
+*/
+module.exports = async (event) => {
+  let result = {slack: {}};
+
+  console.log(`Running [Slack → Retrieve Channel, DM, or Group DM by id]...`);
+  result.slack.channel = await lib.slack.conversations['@0.2.14'].info({
+    id: `${event.channel_id}`
+  });
+
+  console.log(`Running [Slack → Retrieve a User]...`);
+  result.slack.user = await lib.slack.users['@0.3.34'].retrieve({
+    user: `${event.user_id}`
+  });
+
+  console.log(`Running [Slack → Send a Message to a Channel]...`);
+  result.slack.messageResponse = await lib.slack.channels['@0.6.7'].messages.create({
+    channel: `#${result.slack.channel.name}`,
+    text: `Hello <@${result.slack.user.name}>! You said: ${event.text}`
+  });
+
+  return result;
+};
---
+++ functions/events/slack/member_joined_channel.js
@@ -0,0 +1,28 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+/**
+* An HTTP endpoint that acts as a webhook for Slack member_joined_channel event
+* @param {object} event
+* @returns {object} result Your return value
+*/
+module.exports = async (event) => {
+  let result = {slack: {}};
+
+  console.log(`Running [Slack → Retrieve Channel, DM, or Group DM by id]...`);
+  result.slack.channel = await lib.slack.conversations['@0.2.14'].info({
+    id: `${event.event.channel}`
+  });
+
+  console.log(`Running [Slack → Retrieve a User]...`);
+  result.slack.user = await lib.slack.users['@0.3.34'].retrieve({
+    user: `${event.event.user}`
+  });
+
+  console.log(`Welcoming the user to the joined channel`)
+  result.slack.messageResponse = await lib.slack.channels['@0.6.7'].messages.create({
+    channel: `#${result.slack.channel.name}`,
+    text: `Welcome to channel #${result.slack.channel.name}, <@${result.slack.user.name}>!`
+  });
+
+  return result;
+};
